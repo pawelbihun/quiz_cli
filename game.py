9324f7c1a2cad874f32ad44d92459f71147b6d93
Paweł Bihun <pawelbihun@gmail.com>
2023-04-30 21:45:37 +0200

    added showing all questions where player made mistakes

--- game.py
+++ game.py
@@ -8,11 +8,16 @@ class Game:
         self.questions_list: list = []
         self.score: int = 0
         self.player_name: str = 'Nameless player'
+        self.wrong_answers_list: list = []
+        self.count_questions: int = 0
 
     def are_questions_loaded(self) -> bool:
         if len(self.questions_list) > 0:
             return True
         return False
+
+    def count_questions_list(self) -> None:
+        self.count_questions = len(self.questions_list)
 
     def load_questions(self, data: list):
         for obj in data:
@@ -29,19 +34,29 @@ class Game:
         if self.are_questions_loaded():
             print('\n>>>QUIZ START<<<')
             random.shuffle(self.questions_list)
+            self.count_questions_list()
             for count, question in enumerate(self.questions_list, start=1):
                 question.show_with_answers(count)
                 answer = input('Your answer: ')
                 if self.is_score_granted(answer, question.right_answer):
                     self.score += 1
+                else:
+                    question.add_player_answer_to_question(answer)
+                    self.wrong_answers_list.append(question)
 
             print('QUIZ END\n')
             self.player_name = input('Type your name: ')
-            print(f'\n{self.player_name} you scored {self.score} out of {len(self.questions_list)}')
-            if self.score > 0:
-                print('Congratulation!\n\n')
-            else:
-                print('Sorry, you didn\'t score any points. :(')
+            print(f'\n{self.player_name} you scored {self.score} out of {self.count_questions}')
+            if self.score == self.count_questions:
+                print('Congratulation! You answered all the questions correctly. Well done!\n\n')
+            elif self.count_questions > self.score > 0:
+                print(f'{self.player_name} you made some mistakes, here they are:')
+                for question in self.wrong_answers_list:
+                    question.show_with_player_answer()
+            elif self.score == 0:
+                print('You are loser, did not earn any points :(')
+                for question in self.wrong_answers_list:
+                    question.show_with_player_answer()
 
         else:
             print('no data loaded')
--- question.py
+++ question.py
@@ -1,5 +1,6 @@
 class Question:
     answers_letters_list = ['a', 'b', 'c', 'd']
+    player_answer = ''
 
     def __init__(self,
                  question: str,
@@ -9,8 +10,18 @@ class Question:
         self.answers = answers
         self.right_answer = right_answer
 
-    def show_with_answers(self, number_of_question):
+    def show_with_answers(self, number_of_question) -> None:
         print('\n', number_of_question, self.question)
         answers = (tuple(zip(self.answers_letters_list, self.answers)))
         for letter, answer in answers:
             print(f'{letter}: {answer}')
+
+    def show_with_player_answer(self):
+        print('\n', self.question)
+        answers = (tuple(zip(self.answers_letters_list, self.answers)))
+        for letter, answer in answers:
+            print(f'{letter}: {answer}')
+        print(f'Right answer: {self.right_answer}\t\tYour answer: {self.player_answer}')
+
+    def add_player_answer_to_question(self, player_answer: str) -> None:
+        self.player_answer = player_answer
